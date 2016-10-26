digipost/digipost-api-client-java

refactor of UserId
- implemented UserId with the JustA-pattern from digipost/digg
- consequently using UserId

src/main/java/no/digipost/api/client/userdocuments/JustAValid.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ /*
+ * An implementation of the JustA-pattern from github.com/digipost/digg,
+ * with added support for validation, that will also work with JDK7
+ *
+ */
+package no.digipost.api.client.userdocuments;
+
+import java.util.Objects;
+
+public abstract class JustAValid<T> {
+
+    protected final T value;
+
+    private final String description;
+
+    protected JustAValid(T value, String description, String invalidMessage) {
+        if (!isValid(value)) {
+            throw new IllegalArgumentException("Invalid value " + value + " for " + description + " (" + getClass() + "): " + invalidMessage);
+        }
+        this.value = value;
+        this.description = description;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj instanceof JustAValid && getClass().isInstance(obj)) {
+            JustAValid<?> that = (JustAValid<?>) obj;
+            return Objects.equals(this.value, that.value);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return description + " '" + value.toString() + "'";
+    }
+
+    public abstract String serialize();
+
+    public abstract boolean isValid(T value);
+}

src/main/java/no/digipost/api/client/userdocuments/JustAValidXmlAdapter.java
@@ -1,0 +1,29 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public abstract class JustAValidXmlAdapter<T extends JustAValid<String>> extends XmlAdapter<String,T> {
+
+        @Override
+        public abstract T unmarshal(final String value);
+
+        @Override
+        public String marshal(final T item) {
+                return item.serialize();
+        }
+}

src/main/java/no/digipost/api/client/userdocuments/UserIdXmlAdapter.java
@@ -1,0 +1,23 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class UserIdXmlAdapter extends JustAValidXmlAdapter<UserId> {
+    @Override
+    public UserId unmarshal(String value) {
+        return new UserId(value);
+    }
+}

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/JustAValid.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ /*
+ * An implementation of the JustA-pattern from github.com/digipost/digg,
+ * with added support for validation, that will also work with JDK7
+ *
+ */
+package no.digipost.api.useragreements.client;
+
+import java.util.Objects;
+
+public abstract class JustAValid<T> {
+
+    protected final T value;
+
+    private final String description;
+
+    protected JustAValid(T value, String description, String invalidMessage) {
+        if (!isValid(value)) {
+            throw new IllegalArgumentException("Invalid value " + value + " for " + description + " (" + getClass() + "): " + invalidMessage);
+        }
+        this.value = value;
+        this.description = description;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj instanceof JustAValid && getClass().isInstance(obj)) {
+            JustAValid<?> that = (JustAValid<?>) obj;
+            return Objects.equals(this.value, that.value);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return description + " '" + value.toString() + "'";
+    }
+
+    public abstract String serialize();
+
+    public abstract boolean isValid(T value);
+}

src/main/java/no/digipost/api/useragreements/client/JustAValidXmlAdapter.java
@@ -1,0 +1,29 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public abstract class JustAValidXmlAdapter<T extends JustAValid<String>> extends XmlAdapter<String,T> {
+
+        @Override
+        public abstract T unmarshal(final String value);
+
+        @Override
+        public String marshal(final T item) {
+                return item.serialize();
+        }
+}

src/main/java/no/digipost/api/useragreements/client/UserIdXmlAdapter.java
@@ -1,0 +1,23 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class UserIdXmlAdapter extends JustAValidXmlAdapter<UserId> {
+    @Override
+    public UserId unmarshal(String value) {
+        return new UserId(value);
+    }
+}