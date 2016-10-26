digipost/digipost-api-client-java

Improved http request handling and error handling

src/main/java/no/digipost/api/client/userdocuments/Error.java
@@ -1,0 +1,80 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+
+import no.digipost.api.client.representations.ErrorMessage;
+
+import java.util.Objects;
+
+public class Error {
+
+	public static final Error DOCUMENT_NOT_FOUND = new Error("DOCUMENT_NOT_FOUND", "Document not found");
+	public static final Error AGREEMENT_TYPE_NOT_AVAILABLE = new Error("AGREEMENT_TYPE_NOT_AVAILABLE", "Sepcified agreement type is not available");
+	public static final Error UNKNOWN_USER_ID = new Error("UNKNOWN_USER_ID", "The user-id is not a Digipost user");
+	public static final Error NOT_AUTHORIZED = new Error("NOT_AUTHORIZED", "Not authorized");
+	public static final Error AGREEMENT_NOT_FOUND = new Error("AGREEMENT_NOT_FOUND", "Agreement not fount");
+	public static final Error AGREEMENT_DELETED = new Error("AGREEMENT_DELETED", "Agreement has bee deleted by the user");
+
+	private final String code;
+	private final String message;
+
+	private Error(final String code, final String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public Error withCustomMessage(String message) {
+		return new Error(code, message);
+	}
+
+	public static Error fromErrorMessage(final ErrorMessage errorMessage) {
+		return new Error(errorMessage.getErrorCode(), errorMessage.getErrorMessage());
+	}
+
+	public boolean is(final Error other) {
+		return this.code.equalsIgnoreCase(other.code);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Error{");
+		sb.append("code='").append(code).append('\'');
+		sb.append(", message='").append(message).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		return is((Error) o);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(code);
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/GetAgreementResult.java
@@ -1,0 +1,45 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class GetAgreementResult {
+
+	public enum FailedReason {
+		UNKNOWN_USER, NO_AGREEMENT, AGREEMENT_DELETED
+	}
+
+	private final Result<Agreement, FailedReason> result;
+
+	public GetAgreementResult(final Agreement agreement) {
+		this.result = new Result.Success<>(agreement);
+	}
+
+	public GetAgreementResult(final FailedReason failedReason) {
+		this.result = new Result.Failure<>(failedReason);
+	}
+
+	public boolean isSuccess() {
+		return result.isSuccess();
+	}
+
+	public Agreement getAgreement() {
+		return result.getValue();
+	}
+
+	public FailedReason getFailedReason() {
+		return result.getError();
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/Result.java
@@ -1,0 +1,120 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public abstract class Result<V, E> {
+
+	private Result() {}
+
+	public abstract boolean isSuccess();
+
+	public abstract V getValue();
+
+	public abstract E getError();
+
+	static final class Success<V, E> extends Result<V, E> {
+		private final V value;
+
+		public Success(final V value) {
+			Objects.requireNonNull(value);
+			this.value = value;
+		}
+
+		@Override
+		public boolean isSuccess() {
+			return true;
+		}
+
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		@Override
+		public E getError() {
+			throw new NoSuchElementException("No error value. It is a programming error to call getError() without checking isSuccess()");
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			final Success<?, ?> success = (Success<?, ?>) o;
+			return Objects.equals(value, success.value);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(value);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("Success{");
+			sb.append("value=").append(value);
+			sb.append('}');
+			return sb.toString();
+		}
+	}
+
+	static final class Failure<V, E> extends Result<V, E> {
+		private final E error;
+
+		public Failure(final E error) {
+			Objects.requireNonNull(error);
+			this.error = error;
+		}
+
+		@Override
+		public boolean isSuccess() {
+			return false;
+		}
+
+		@Override
+		public V getValue() {
+			throw new NoSuchElementException("No value. It is a programming error to call getValue() without checking isSuccess()");
+		}
+
+		@Override
+		public E getError() {
+			return error;
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			final Failure<?, ?> failure = (Failure<?, ?>) o;
+			return Objects.equals(error, failure.error);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(error);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("Failure{");
+			sb.append("error=").append(error);
+			sb.append('}');
+			return sb.toString();
+		}
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/RuntimeIOException.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class RuntimeIOException extends UserDocumentsApiException {
+
+	public RuntimeIOException(final String message) {
+		super(message);
+	}
+
+	public RuntimeIOException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public RuntimeIOException(final Throwable cause) {
+		super(cause);
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/UnexpectedResponseException.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import org.apache.http.StatusLine;
+
+public class UnexpectedResponseException extends UserDocumentsApiException {
+	private final Error error;
+	private final String rawBody;
+
+	public UnexpectedResponseException(final StatusLine status, final Error error) {
+		super(String.format("Unexpected response: status [%s - %s], error [%s - %s]", status.getStatusCode(), status.getReasonPhrase(), error.getCode(), error.getMessage()));
+		this.error = error;
+		this.rawBody = null;
+	}
+
+	public UnexpectedResponseException(final StatusLine status, final Exception cause) {
+		this(status, null, cause);
+	}
+
+	public UnexpectedResponseException(final StatusLine status, final String body) {
+		this(status, body, null);
+	}
+
+	public UnexpectedResponseException(final StatusLine status, final String body, final Exception cause) {
+		super(String.format("Unexpected response: status [%s - %s], response body [%s - %s]", status.getStatusCode(), status.getReasonPhrase(), body), cause);
+		this.error = null;
+		this.rawBody = body;
+	}
+
+	public Error getError() {
+		return error;
+	}
+
+	public String getRawBody() {
+		return rawBody;
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/UserDocumentsApiException.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class UserDocumentsApiException extends RuntimeException {
+
+	public UserDocumentsApiException(final String message) {
+		super(message);
+	}
+
+	public UserDocumentsApiException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public UserDocumentsApiException(final Throwable cause) {
+		super(cause);
+	}
+}

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/Error.java
@@ -1,0 +1,80 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+
+import no.digipost.api.useragreements.client.representations.ErrorMessage;
+
+import java.util.Objects;
+
+public class Error {
+
+	public static final Error DOCUMENT_NOT_FOUND = new Error("DOCUMENT_NOT_FOUND", "Document not found");
+	public static final Error AGREEMENT_TYPE_NOT_AVAILABLE = new Error("AGREEMENT_TYPE_NOT_AVAILABLE", "Sepcified agreement type is not available");
+	public static final Error UNKNOWN_USER_ID = new Error("UNKNOWN_USER_ID", "The user-id is not a Digipost user");
+	public static final Error NOT_AUTHORIZED = new Error("NOT_AUTHORIZED", "Not authorized");
+	public static final Error AGREEMENT_NOT_FOUND = new Error("AGREEMENT_NOT_FOUND", "Agreement not fount");
+	public static final Error AGREEMENT_DELETED = new Error("AGREEMENT_DELETED", "Agreement has bee deleted by the user");
+
+	private final String code;
+	private final String message;
+
+	private Error(final String code, final String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public Error withCustomMessage(String message) {
+		return new Error(code, message);
+	}
+
+	public static Error fromErrorMessage(final ErrorMessage errorMessage) {
+		return new Error(errorMessage.getErrorCode(), errorMessage.getErrorMessage());
+	}
+
+	public boolean is(final Error other) {
+		return this.code.equalsIgnoreCase(other.code);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Error{");
+		sb.append("code='").append(code).append('\'');
+		sb.append(", message='").append(message).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		return is((Error) o);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(code);
+	}
+}

src/main/java/no/digipost/api/useragreements/client/GetAgreementResult.java
@@ -1,0 +1,45 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class GetAgreementResult {
+
+	public enum FailedReason {
+		UNKNOWN_USER, NO_AGREEMENT, AGREEMENT_DELETED
+	}
+
+	private final Result<Agreement, FailedReason> result;
+
+	public GetAgreementResult(final Agreement agreement) {
+		this.result = new Result.Success<>(agreement);
+	}
+
+	public GetAgreementResult(final FailedReason failedReason) {
+		this.result = new Result.Failure<>(failedReason);
+	}
+
+	public boolean isSuccess() {
+		return result.isSuccess();
+	}
+
+	public Agreement getAgreement() {
+		return result.getValue();
+	}
+
+	public FailedReason getFailedReason() {
+		return result.getError();
+	}
+}

src/main/java/no/digipost/api/useragreements/client/Result.java
@@ -1,0 +1,120 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public abstract class Result<V, E> {
+
+	private Result() {}
+
+	public abstract boolean isSuccess();
+
+	public abstract V getValue();
+
+	public abstract E getError();
+
+	static final class Success<V, E> extends Result<V, E> {
+		private final V value;
+
+		public Success(final V value) {
+			Objects.requireNonNull(value);
+			this.value = value;
+		}
+
+		@Override
+		public boolean isSuccess() {
+			return true;
+		}
+
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		@Override
+		public E getError() {
+			throw new NoSuchElementException("No error value. It is a programming error to call getError() without checking isSuccess()");
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			final Success<?, ?> success = (Success<?, ?>) o;
+			return Objects.equals(value, success.value);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(value);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("Success{");
+			sb.append("value=").append(value);
+			sb.append('}');
+			return sb.toString();
+		}
+	}
+
+	static final class Failure<V, E> extends Result<V, E> {
+		private final E error;
+
+		public Failure(final E error) {
+			Objects.requireNonNull(error);
+			this.error = error;
+		}
+
+		@Override
+		public boolean isSuccess() {
+			return false;
+		}
+
+		@Override
+		public V getValue() {
+			throw new NoSuchElementException("No value. It is a programming error to call getValue() without checking isSuccess()");
+		}
+
+		@Override
+		public E getError() {
+			return error;
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			final Failure<?, ?> failure = (Failure<?, ?>) o;
+			return Objects.equals(error, failure.error);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(error);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder("Failure{");
+			sb.append("error=").append(error);
+			sb.append('}');
+			return sb.toString();
+		}
+	}
+}

src/main/java/no/digipost/api/useragreements/client/RuntimeIOException.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class RuntimeIOException extends UserDocumentsApiException {
+
+	public RuntimeIOException(final String message) {
+		super(message);
+	}
+
+	public RuntimeIOException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public RuntimeIOException(final Throwable cause) {
+		super(cause);
+	}
+}

src/main/java/no/digipost/api/useragreements/client/UnexpectedResponseException.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import org.apache.http.StatusLine;
+
+public class UnexpectedResponseException extends UserDocumentsApiException {
+	private final Error error;
+	private final String rawBody;
+
+	public UnexpectedResponseException(final StatusLine status, final Error error) {
+		super(String.format("Unexpected response: status [%s - %s], error [%s - %s]", status.getStatusCode(), status.getReasonPhrase(), error.getCode(), error.getMessage()));
+		this.error = error;
+		this.rawBody = null;
+	}
+
+	public UnexpectedResponseException(final StatusLine status, final Exception cause) {
+		this(status, null, cause);
+	}
+
+	public UnexpectedResponseException(final StatusLine status, final String body) {
+		this(status, body, null);
+	}
+
+	public UnexpectedResponseException(final StatusLine status, final String body, final Exception cause) {
+		super(String.format("Unexpected response: status [%s - %s], response body [%s - %s]", status.getStatusCode(), status.getReasonPhrase(), body), cause);
+		this.error = null;
+		this.rawBody = body;
+	}
+
+	public Error getError() {
+		return error;
+	}
+
+	public String getRawBody() {
+		return rawBody;
+	}
+}

src/main/java/no/digipost/api/useragreements/client/UserDocumentsApiException.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class UserDocumentsApiException extends RuntimeException {
+
+	public UserDocumentsApiException(final String message) {
+		super(message);
+	}
+
+	public UserDocumentsApiException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	public UserDocumentsApiException(final Throwable cause) {
+		super(cause);
+	}
+}