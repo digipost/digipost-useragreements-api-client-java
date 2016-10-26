digipost/digipost-api-client-java

Added sms-notification flag for createAgreement

src/main/java/no/digipost/api/client/userdocuments/package-info.java
@@ -1,0 +1,17 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.digipost.no/schema/v6", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package no.digipost.api.client.userdocuments;

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/package-info.java
@@ -1,0 +1,17 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.digipost.no/schema/v6", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package no.digipost.api.useragreements.client;