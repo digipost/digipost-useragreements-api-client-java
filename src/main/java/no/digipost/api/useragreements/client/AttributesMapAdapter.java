digipost/digipost-api-client-java

Attributes map on agreement

src/main/java/no/digipost/api/client/userdocuments/AttributesMapAdapter.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AttributesMapAdapter extends XmlAdapter<Attributes, HashMap<String, String>> {
+
+	@Override
+	public HashMap<String, String> unmarshal(final Attributes v) throws Exception {
+		final HashMap<String, String> map = new HashMap<>();
+		for (Attribute attribute : v.getAttributes()) {
+			map.put(attribute.getKey(), attribute.getValue());
+		}
+		return map;
+	}
+
+	@Override
+	public Attributes marshal(final HashMap<String, String> v) throws Exception {
+		final ArrayList<Attribute> list = new ArrayList<>();
+		for (Map.Entry<String, String> entry : v.entrySet()) {
+			list.add(new Attribute(entry.getKey(), entry.getValue()));
+		}
+		return new Attributes(list);
+	}
+}

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/AttributesMapAdapter.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AttributesMapAdapter extends XmlAdapter<Attributes, HashMap<String, String>> {
+
+	@Override
+	public HashMap<String, String> unmarshal(final Attributes v) throws Exception {
+		final HashMap<String, String> map = new HashMap<>();
+		for (Attribute attribute : v.getAttributes()) {
+			map.put(attribute.getKey(), attribute.getValue());
+		}
+		return map;
+	}
+
+	@Override
+	public Attributes marshal(final HashMap<String, String> v) throws Exception {
+		final ArrayList<Attribute> list = new ArrayList<>();
+		for (Map.Entry<String, String> entry : v.entrySet()) {
+			list.add(new Attribute(entry.getKey(), entry.getValue()));
+		}
+		return new Attributes(list);
+	}
+}