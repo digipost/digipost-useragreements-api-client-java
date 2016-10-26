digipost/digipost-api-client-java

implemented Kid as a type with the JustA-pattern

src/main/java/no/digipost/api/client/userdocuments/Kid.java
@@ -1,0 +1,37 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class Kid extends JustAValid<String> {
+
+    protected Kid(String value) {
+        super(value, "Kid", "Kid can not be null");
+    }
+
+    public static Kid of(String kid) {
+        return new Kid(kid);
+    }
+
+    @Override
+    public String serialize() {
+        return value;
+    }
+
+    @Override
+    public boolean isValid(String value) {
+        return value != null;
+    }
+}

src/main/java/no/digipost/api/client/userdocuments/KidXmlAdapter.java
@@ -1,0 +1,23 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class KidXmlAdapter extends JustAValidXmlAdapter<Kid> {
+    @Override
+    public Kid unmarshal(String value) {
+        return new Kid(value);
+    }
+}

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/Kid.java
@@ -1,0 +1,37 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class Kid extends JustAValid<String> {
+
+    protected Kid(String value) {
+        super(value, "Kid", "Kid can not be null");
+    }
+
+    public static Kid of(String kid) {
+        return new Kid(kid);
+    }
+
+    @Override
+    public String serialize() {
+        return value;
+    }
+
+    @Override
+    public boolean isValid(String value) {
+        return value != null;
+    }
+}

src/main/java/no/digipost/api/useragreements/client/KidXmlAdapter.java
@@ -1,0 +1,23 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class KidXmlAdapter extends JustAValidXmlAdapter<Kid> {
+    @Override
+    public Kid unmarshal(String value) {
+        return new Kid(value);
+    }
+}