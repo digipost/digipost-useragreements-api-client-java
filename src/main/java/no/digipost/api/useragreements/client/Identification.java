digipost/digipost-api-client-java

Rettet feil i identifyUser

src/main/java/no/digipost/api/client/userdocuments/Identification.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import no.digipost.api.client.representations.PersonalIdentificationNumber;
+
+import javax.xml.bind.annotation.*;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "identification", propOrder = {
+		"personalIdentificationNumber"
+})
+@XmlRootElement(name = "identification")
+public class Identification {
+
+	@XmlElement(name = "personal-identification-number", nillable = false)
+	protected String personalIdentificationNumber;
+
+	public Identification(final PersonalIdentificationNumber personalIdentificationNumber) {
+		this.personalIdentificationNumber = personalIdentificationNumber.asString();
+	}
+
+	//JAXB
+	public Identification() {
+
+	}
+
+	public String getPersonalIdentificationNumber() {
+		return personalIdentificationNumber;
+	}
+
+	@Override
+	public String toString() {
+		return personalIdentificationNumber;
+	}
+}
+

src/main/java/no/digipost/api/client/userdocuments/IdentificationResult.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import no.digipost.api.client.representations.InvalidReason;
+import no.digipost.api.client.representations.UnidentifiedReason;
+
+import javax.xml.bind.annotation.*;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "identification-result", propOrder = {
+		"result",
+		"invalidReason",
+		"unidentifiedReason"
+})
+@XmlRootElement(name = "identification-result")
+public class IdentificationResult {
+
+	@XmlElement(required = true)
+	protected IdentificationResultCode result;
+	@XmlElement(name = "invalid-reason")
+	protected InvalidReason invalidReason;
+	@XmlElement(name = "unidentified-reason")
+	protected UnidentifiedReason unidentifiedReason;
+
+	public IdentificationResult() {
+	}
+
+	public IdentificationResultCode getResult() {
+		return result;
+	}
+
+	public InvalidReason getInvalidReason() {
+		return invalidReason;
+	}
+
+	public UnidentifiedReason getUnidentifiedReason() {
+		return unidentifiedReason;
+	}
+
+	public static IdentificationResult digipost() {
+		IdentificationResult result = new IdentificationResult();
+		result.result = IdentificationResultCode.DIGIPOST;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "IdentificationResult{" +
+				"result=" + result +
+				", invalidReason=" + invalidReason +
+				", unidentifiedReason=" + unidentifiedReason +
+				'}';
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/IdentificationResultCode.java
@@ -1,0 +1,38 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "identification-result-code")
+@XmlEnum
+public enum IdentificationResultCode {
+
+	DIGIPOST,
+	IDENTIFIED,
+	UNIDENTIFIED,
+	INVALID;
+
+	public String value() {
+		return name();
+	}
+
+	public static IdentificationResultCode fromValue(String v) {
+		return valueOf(v);
+	}
+
+}

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/Identification.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import no.digipost.api.useragreements.client.representations.PersonalIdentificationNumber;
+
+import javax.xml.bind.annotation.*;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "identification", propOrder = {
+		"personalIdentificationNumber"
+})
+@XmlRootElement(name = "identification")
+public class Identification {
+
+	@XmlElement(name = "personal-identification-number", nillable = false)
+	protected String personalIdentificationNumber;
+
+	public Identification(final PersonalIdentificationNumber personalIdentificationNumber) {
+		this.personalIdentificationNumber = personalIdentificationNumber.asString();
+	}
+
+	//JAXB
+	public Identification() {
+
+	}
+
+	public String getPersonalIdentificationNumber() {
+		return personalIdentificationNumber;
+	}
+
+	@Override
+	public String toString() {
+		return personalIdentificationNumber;
+	}
+}
+

src/main/java/no/digipost/api/useragreements/client/IdentificationResult.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import no.digipost.api.useragreements.client.representations.InvalidReason;
+import no.digipost.api.useragreements.client.representations.UnidentifiedReason;
+
+import javax.xml.bind.annotation.*;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "identification-result", propOrder = {
+		"result",
+		"invalidReason",
+		"unidentifiedReason"
+})
+@XmlRootElement(name = "identification-result")
+public class IdentificationResult {
+
+	@XmlElement(required = true)
+	protected IdentificationResultCode result;
+	@XmlElement(name = "invalid-reason")
+	protected InvalidReason invalidReason;
+	@XmlElement(name = "unidentified-reason")
+	protected UnidentifiedReason unidentifiedReason;
+
+	public IdentificationResult() {
+	}
+
+	public IdentificationResultCode getResult() {
+		return result;
+	}
+
+	public InvalidReason getInvalidReason() {
+		return invalidReason;
+	}
+
+	public UnidentifiedReason getUnidentifiedReason() {
+		return unidentifiedReason;
+	}
+
+	public static IdentificationResult digipost() {
+		IdentificationResult result = new IdentificationResult();
+		result.result = IdentificationResultCode.DIGIPOST;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "IdentificationResult{" +
+				"result=" + result +
+				", invalidReason=" + invalidReason +
+				", unidentifiedReason=" + unidentifiedReason +
+				'}';
+	}
+}

src/main/java/no/digipost/api/useragreements/client/IdentificationResultCode.java
@@ -1,0 +1,38 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "identification-result-code")
+@XmlEnum
+public enum IdentificationResultCode {
+
+	DIGIPOST,
+	IDENTIFIED,
+	UNIDENTIFIED,
+	INVALID;
+
+	public String value() {
+		return name();
+	}
+
+	public static IdentificationResultCode fromValue(String v) {
+		return valueOf(v);
+	}
+
+}