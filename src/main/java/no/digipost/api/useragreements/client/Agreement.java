digipost/digipost-api-client-java

New client for user documents

src/main/java/no/digipost/api/client/userdocuments/Agreement.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Map;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Agreement {
+	@XmlElement(required = true)
+	private String type;
+	@XmlElement(name = "user-id", required = true)
+	private String userId;
+	//private String sessionId;
+	//TODO: Include BankId session identifier?
+	@XmlElement(required = false)
+	private Map<String, String> attrbutes;
+
+	public Agreement() {}
+
+	public Agreement(final String type, final String userId) {
+		this.type = type;
+		this.userId = userId;
+	}
+
+	public static Agreement createInvoiceBankAgreement(final String userId) {
+		return new Agreement("invoice-bank", userId);
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/ApiService.java
@@ -1,0 +1,121 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import no.digipost.api.client.errorhandling.DigipostClientException;
+import no.digipost.api.client.representations.EntryPoint;
+import no.digipost.api.client.representations.ErrorMessage;
+import no.digipost.api.client.representations.Identification;
+import no.digipost.cache.inmemory.SingleCached;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import javax.xml.bind.JAXB;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.Callable;
+
+import static no.digipost.api.client.Headers.X_Digipost_UserId;
+import static no.digipost.api.client.representations.MediaTypes.DIGIPOST_MEDIA_TYPE_V6;
+import static no.digipost.cache.inmemory.CacheConfig.expireAfterAccess;
+import static no.digipost.cache.inmemory.CacheConfig.useSoftValues;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.joda.time.Duration.standardMinutes;
+
+public class ApiService {
+
+	private static final String ROOT = "/";
+	private static final String USER_DOCUMENTS = "/user-documents";
+	private static final String USER_AGREEMENTS = "/user-agreements";
+
+	private final URI serviceEndpoint;
+	private final long accountId;
+	private final CloseableHttpClient httpClient;
+
+	public ApiService(final URI serviceEndpoint, final long accountId, final CloseableHttpClient httpClient) {
+		this.serviceEndpoint = serviceEndpoint;
+		this.accountId = accountId;
+		this.httpClient = httpClient;
+	}
+
+	public CloseableHttpResponse identifyRecipient(final Identification identification) {
+		HttpPost httpPost = prepareHttpPost(getEntryPoint().getIdentificationUri().getPath());
+		httpPost.setEntity(marshallJaxbEntity(identification));
+		return send(httpPost);
+	}
+
+	public CloseableHttpResponse createAgreement(final Agreement agreement) {
+		HttpPost httpPost = prepareHttpPost(USER_AGREEMENTS);
+		httpPost.setEntity(marshallJaxbEntity(agreement));
+		return send(httpPost);
+	}
+
+	private HttpPost prepareHttpPost(final String path) {
+		HttpPost httpPost = new HttpPost(serviceEndpoint.resolve(path));
+		httpPost.setHeader(HttpHeaders.ACCEPT, DIGIPOST_MEDIA_TYPE_V6);
+		httpPost.setHeader(HttpHeaders.CONTENT_TYPE, DIGIPOST_MEDIA_TYPE_V6);
+		return httpPost;
+	}
+
+	private HttpEntity marshallJaxbEntity(final Object obj) {
+		ByteArrayOutputStream bao = new ByteArrayOutputStream();
+		JAXB.marshal(obj, bao);
+		return new ByteArrayEntity(bao.toByteArray());
+	}
+
+	EntryPoint getEntryPoint() {
+		return cachedEntryPoint.get();
+	}
+
+	private CloseableHttpResponse send(HttpRequestBase request){
+		try {
+			request.setHeader(X_Digipost_UserId, String.valueOf(accountId));
+			return httpClient.execute(request);
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+
+	private final Callable<EntryPoint> entryPoint = new Callable<EntryPoint>() {
+		@Override
+		public EntryPoint call() throws Exception {
+
+			HttpGet httpGet = new HttpGet(serviceEndpoint.resolve(ROOT));
+			httpGet.setHeader(HttpHeaders.ACCEPT, DIGIPOST_MEDIA_TYPE_V6);
+
+			try(CloseableHttpResponse execute = send(httpGet)) {
+				if (execute.getStatusLine().getStatusCode() == SC_OK) {
+					EntryPoint entryPoint = JAXB.unmarshal(execute.getEntity().getContent(), EntryPoint.class);
+					return entryPoint;
+				} else {
+					ErrorMessage errorMessage = JAXB.unmarshal(execute.getEntity().getContent(), ErrorMessage.class);
+					throw new DigipostClientException(errorMessage);
+				}
+			}
+
+		}
+	};
+
+	private final SingleCached<EntryPoint> cachedEntryPoint = new SingleCached<>("digipost-entrypoint", entryPoint, expireAfterAccess(standardMinutes(5)), useSoftValues);
+}

src/main/java/no/digipost/api/client/userdocuments/ApiServiceProvider.java
@@ -1,0 +1,32 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+class ApiServiceProvider {
+
+	private ApiService apiService;
+
+	ApiService getApiService() {
+		if (apiService == null) {
+			throw new RuntimeException("ApiService not initialized");
+		}
+		return apiService;
+	}
+
+	void setApiService(final ApiService apiService) {
+		this.apiService = apiService;
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/Document.java
@@ -1,0 +1,27 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class Document {
+
+	private final long id;
+	private final Invoice invoice;
+
+	public Document(final long id, final Invoice invoice) {
+		this.id = id;
+		this.invoice = invoice;
+	}
+}

src/main/java/no/digipost/api/client/userdocuments/Invoice.java
@@ -1,0 +1,19 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+public class Invoice {
+}

digipost/digipost-useragreements-api-client-java

src/main/java/no/digipost/api/useragreements/client/Agreement.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Map;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Agreement {
+	@XmlElement(required = true)
+	private String type;
+	@XmlElement(name = "user-id", required = true)
+	private String userId;
+	//private String sessionId;
+	//TODO: Include BankId session identifier?
+	@XmlElement(required = false)
+	private Map<String, String> attrbutes;
+
+	public Agreement() {}
+
+	public Agreement(final String type, final String userId) {
+		this.type = type;
+		this.userId = userId;
+	}
+
+	public static Agreement createInvoiceBankAgreement(final String userId) {
+		return new Agreement("invoice-bank", userId);
+	}
+}

src/main/java/no/digipost/api/useragreements/client/ApiService.java
@@ -1,0 +1,121 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import no.digipost.api.useragreements.client.errorhandling.DigipostClientException;
+import no.digipost.api.useragreements.client.representations.EntryPoint;
+import no.digipost.api.useragreements.client.representations.ErrorMessage;
+import no.digipost.api.useragreements.client.representations.Identification;
+import no.digipost.cache.inmemory.SingleCached;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import javax.xml.bind.JAXB;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.Callable;
+
+import static no.digipost.api.useragreements.client.Headers.X_Digipost_UserId;
+import static no.digipost.api.useragreements.client.representations.MediaTypes.DIGIPOST_MEDIA_TYPE_V6;
+import static no.digipost.cache.inmemory.CacheConfig.expireAfterAccess;
+import static no.digipost.cache.inmemory.CacheConfig.useSoftValues;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.joda.time.Duration.standardMinutes;
+
+public class ApiService {
+
+	private static final String ROOT = "/";
+	private static final String USER_DOCUMENTS = "/user-documents";
+	private static final String USER_AGREEMENTS = "/user-agreements";
+
+	private final URI serviceEndpoint;
+	private final long accountId;
+	private final CloseableHttpClient httpClient;
+
+	public ApiService(final URI serviceEndpoint, final long accountId, final CloseableHttpClient httpClient) {
+		this.serviceEndpoint = serviceEndpoint;
+		this.accountId = accountId;
+		this.httpClient = httpClient;
+	}
+
+	public CloseableHttpResponse identifyRecipient(final Identification identification) {
+		HttpPost httpPost = prepareHttpPost(getEntryPoint().getIdentificationUri().getPath());
+		httpPost.setEntity(marshallJaxbEntity(identification));
+		return send(httpPost);
+	}
+
+	public CloseableHttpResponse createAgreement(final Agreement agreement) {
+		HttpPost httpPost = prepareHttpPost(USER_AGREEMENTS);
+		httpPost.setEntity(marshallJaxbEntity(agreement));
+		return send(httpPost);
+	}
+
+	private HttpPost prepareHttpPost(final String path) {
+		HttpPost httpPost = new HttpPost(serviceEndpoint.resolve(path));
+		httpPost.setHeader(HttpHeaders.ACCEPT, DIGIPOST_MEDIA_TYPE_V6);
+		httpPost.setHeader(HttpHeaders.CONTENT_TYPE, DIGIPOST_MEDIA_TYPE_V6);
+		return httpPost;
+	}
+
+	private HttpEntity marshallJaxbEntity(final Object obj) {
+		ByteArrayOutputStream bao = new ByteArrayOutputStream();
+		JAXB.marshal(obj, bao);
+		return new ByteArrayEntity(bao.toByteArray());
+	}
+
+	EntryPoint getEntryPoint() {
+		return cachedEntryPoint.get();
+	}
+
+	private CloseableHttpResponse send(HttpRequestBase request){
+		try {
+			request.setHeader(X_Digipost_UserId, String.valueOf(accountId));
+			return httpClient.execute(request);
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+
+	private final Callable<EntryPoint> entryPoint = new Callable<EntryPoint>() {
+		@Override
+		public EntryPoint call() throws Exception {
+
+			HttpGet httpGet = new HttpGet(serviceEndpoint.resolve(ROOT));
+			httpGet.setHeader(HttpHeaders.ACCEPT, DIGIPOST_MEDIA_TYPE_V6);
+
+			try(CloseableHttpResponse execute = send(httpGet)) {
+				if (execute.getStatusLine().getStatusCode() == SC_OK) {
+					EntryPoint entryPoint = JAXB.unmarshal(execute.getEntity().getContent(), EntryPoint.class);
+					return entryPoint;
+				} else {
+					ErrorMessage errorMessage = JAXB.unmarshal(execute.getEntity().getContent(), ErrorMessage.class);
+					throw new DigipostClientException(errorMessage);
+				}
+			}
+
+		}
+	};
+
+	private final SingleCached<EntryPoint> cachedEntryPoint = new SingleCached<>("digipost-entrypoint", entryPoint, expireAfterAccess(standardMinutes(5)), useSoftValues);
+}

src/main/java/no/digipost/api/useragreements/client/ApiServiceProvider.java
@@ -1,0 +1,32 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+class ApiServiceProvider {
+
+	private ApiService apiService;
+
+	ApiService getApiService() {
+		if (apiService == null) {
+			throw new RuntimeException("ApiService not initialized");
+		}
+		return apiService;
+	}
+
+	void setApiService(final ApiService apiService) {
+		this.apiService = apiService;
+	}
+}

src/main/java/no/digipost/api/useragreements/client/Document.java
@@ -1,0 +1,27 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class Document {
+
+	private final long id;
+	private final Invoice invoice;
+
+	public Document(final long id, final Invoice invoice) {
+		this.id = id;
+		this.invoice = invoice;
+	}
+}

src/main/java/no/digipost/api/useragreements/client/Invoice.java
@@ -1,0 +1,19 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+public class Invoice {
+}