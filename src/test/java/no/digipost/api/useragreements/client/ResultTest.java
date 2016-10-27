digipost/digipost-api-client-java

Test error handling in Result.java

src/test/java/no/digipost/api/client/userdocuments/ResultTest.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.userdocuments;
+
+import no.digipost.api.client.util.Supplier;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class ResultTest {
+
+	@Test(expected = NoSuchElementException.class)
+	public void successShouldThrowExcetionOnGetError() {
+		new Result.Success<>("Success!").getError();
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void failureShouldThrowExceptionOnGetValue() {
+		new Result.Failure<>("Failure").getValue();
+	}
+
+	@Test(expected = UserDocumentsApiException.class)
+	public void failureShouldThrowCustomeExceptionOnGetValue() {
+		new Result.Failure<>("Failure", new Supplier<RuntimeException>() {
+			@Override
+			public RuntimeException get() {
+				return new UserDocumentsApiException("Custome exception");
+			}
+		}).getValue();
+	}
+}

digipost/digipost-useragreements-api-client-java

src/test/java/no/digipost/api/useragreements/client/ResultTest.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.useragreements.client;
+
+import no.digipost.api.useragreements.client.util.Supplier;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class ResultTest {
+
+	@Test(expected = NoSuchElementException.class)
+	public void successShouldThrowExcetionOnGetError() {
+		new Result.Success<>("Success!").getError();
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void failureShouldThrowExceptionOnGetValue() {
+		new Result.Failure<>("Failure").getValue();
+	}
+
+	@Test(expected = UserDocumentsApiException.class)
+	public void failureShouldThrowCustomeExceptionOnGetValue() {
+		new Result.Failure<>("Failure", new Supplier<RuntimeException>() {
+			@Override
+			public RuntimeException get() {
+				return new UserDocumentsApiException("Custome exception");
+			}
+		}).getValue();
+	}
+}